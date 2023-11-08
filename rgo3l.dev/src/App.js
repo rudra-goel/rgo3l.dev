5e66ae6704502a9584c8f6daed020dd76fd3f044
Rudra Goel <rgoel@lawn-128-61-72-234.lawn.gatech.edu>
2023-11-08 02:03:20 -0500

    initialized creact app

---
+++ rgo3l.dev/src/App.js
@@ -0,0 +1,22 @@
+import logo from './Fur.jpeg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer">
+          IM GONNA CUM IN AARONS MOUTH
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
