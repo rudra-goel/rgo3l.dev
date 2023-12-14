0bd5135aa97685dfb1d3babf6f9787147dfb27a0
Rudra Goel <rgoel@lawn-128-61-65-75.lawn.gatech.edu>
2023-12-14 17:26:03 -0500

    style navbar some

--- rgo3l.dev/src/components/Navbar.js
+++ rgo3l.dev/src/components/Navbar.js
@@ -1,84 +1,122 @@
 import React from 'react'
+import "./Navbar.css"
 
 export default function Navbar() {
   let page = window.location.href;
   console.log(page)
   page = page.split("/")
   page = page[page.length-1]
+
   switch (page) {
     case "about-me":
       return (
-        <div className="navbar" >
-            <nav>
-                <li><a href="/">Home</a></li>
-                <li><a href="/project-portfolio">Project Portfolio</a></li>
-                <li><a href="/experience">Experience</a></li>
-                <li><a href="/resume">Resume</a></li>
-                <li><a href="/contact">Contact</a></li>
+        <div className="nav-container" >
+            <nav className="navbar">
+              <img alt="RG" id="navbar-logo" src="../../RG.png" width ="75" height="75"></img>
+              <div className="menu-toggle" id="mobile-menu">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+              </div>
+              <ul className="nav-menu">   
+                <li ><a className="nav-links" href="/">Home</a></li>
+                <li ><a className="nav-links" href="/project-portfolio">Project Portfolio</a></li>
+                <li ><a className="nav-links" href="/experience">Experience</a></li>
+                <li ><a className="nav-links"href="/resume">Resume</a></li>
+                <li ><a className="nav-links" href="/contact">Contact</a></li>
+              </ul>
             </nav>
           </div>
       )
-      break;
+      
     case "project-portfolio":
       return (
-        <div className="navbar" >
-            <nav>
-                <li><a href="/">Home</a></li>
-                <li><a href="about-me">About Me</a></li>
-                <li><a href="/experience">Experience</a></li>
-                <li><a href="/resume">Resume</a></li>
-                <li><a href="/contact">Contact</a></li>
+        <div className="nav-container" >
+            <nav className="navbar">
+              <img  alt="RG" id="navbar-logo" src="../../RG.png" width ="75" height="75"></img>
+              <div className="menu-toggle" id="mobile-menu">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+              </div>
+              <ul className="nav-menu">   
+                <li className="nav-links"><a className="nav-links" href="/">Home</a></li>
+                <li><a className="nav-links" href="about-me">About Me</a></li>
+                <li><a className="nav-links" href="/experience">Experience</a></li>
+                <li><a className="nav-links" href="/resume">Resume</a></li>
+                <li><a className="nav-links" href="/contact">Contact</a></li>
+              </ul>
             </nav>
           </div>
       )
-      break;
     case "experience":
       return (
-        <div className="navbar" >
-            <nav>
-                <li><a href="/">Home</a></li>
-                <li><a href="about-me">About Me</a></li>
-                <li><a href="/project-portfolio">Project Portfolio</a></li>
-                <li><a href="/resume">Resume</a></li>
-                <li><a href="/contact">Contact</a></li>
+        <div className="nav-container" >
+            <nav className="navbar">
+              <img  alt="RG" id="navbar-logo" src="../../RG.png" width ="75" height="75"></img>
+              <div className="menu-toggle" id="mobile-menu">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+              </div>
+              <ul className="nav-menu">   
+                <li><a className="nav-links" href="/">Home</a></li>
+                <li><a className="nav-links" href="about-me">About Me</a></li>
+                <li><a className="nav-links" href="/project-portfolio">Project Portfolio</a></li>
+                <li><a className="nav-links" href="/resume">Resume</a></li>
+                <li><a className="nav-links" href="/contact">Contact</a></li>
+              </ul>
             </nav>
           </div>
       )
-      break;
       case "resume":
         return (
-          <div className="navbar" >
-            <nav>
-                <li><a href="/">Home</a></li>
-                <li><a href="about-me">About Me</a></li>
-                <li><a href="/project-portfolio">Project Portfolio</a></li>
-                <li><a href="/experience">Experience</a></li>
-                <li><a href="/contact">Contact</a></li>
+          <div className="nav-container" >
+            <nav className="navbar">
+              <img  alt="RG" id="navbar-logo" src="../../RG.png" width ="75" height="75"></img>
+              <div className="menu-toggle" id="mobile-menu">
+                <span className="bar"></span>
+                <span className="bar"></span>
+                <span className="bar"></span>
+              </div>
+              <ul className="nav-menu">   
+                <li><a className="nav-links" href="/">Home</a></li>
+                <li><a className="nav-links" href="about-me">About Me</a></li>
+                <li><a className="nav-links" href="/project-portfolio">Project Portfolio</a></li>
+                <li><a className="nav-links" href="/experience">Experience</a></li>
+                <li><a className="nav-links" href="/contact">Contact</a></li>
+              </ul>
             </nav>
           </div>
       )
-      break;
       case "contact":
         return (
-          <div className="navbar" >
-            <nav>
-                <li><a href="/">Home</a></li>
-                <li><a href="about-me">About Me</a></li>
-                <li><a href="/project-portfolio">Project Portfolio</a></li>
-                <li><a href="/experience">Resume</a></li>            </nav>
-                <li><a href="/resume">Experience</a></li>
+          <div className="nav-container" >
+            <nav className="navbar">
+              <img alt="RG" id="navbar-logo"  src="../../RG.png" width ="75" height="75"></img>
+                <div className="menu-toggle" id="mobile-menu">
+                  <span className="bar"></span>
+                  <span className="bar"></span>
+                  <span className="bar"></span>
+                </div>
+                <ul className="nav-menu">   
+                  <li><a className="nav-links" href="/">Home</a></li>
+                  <li><a className="nav-links" href="about-me">About Me</a></li>
+                  <li><a className="nav-links" href="/project-portfolio">Project Portfolio</a></li>
+                  <li><a className="nav-links" href="/resume">Experience</a></li>
+                  <li><a className="nav-links" href="/experience">Resume</a></li>  
+                </ul>
+              </nav>
           </div>
       )
-      break;
     
   
     default:
       return (
-        <div className="navbar" >
+        <div className="nav-container" >
             Error in Navbar
           </div>
       )
-      break;
   }
   
 }
